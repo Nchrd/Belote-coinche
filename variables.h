7147b6c7d45d517f7fccdfc31716dad3d4557487
Noé Echard <65755297+Th3PaSt3qu3@users.noreply.github.com>
2020-06-08 16:56:36 +0200

    Create variables.h

---
+++ variables.h
@@ -0,0 +1,53 @@
+int compteur;
+int choix_menu;
+int choix;
+
+typedef enum
+{
+    NONE = -1,
+    PASSE = 0,
+    MISE = 1,
+    CAPOT = 2,
+    GENERAL = 3,
+    COINCHE = 4,
+} type_contrat;
+
+struct Carte
+{
+    int atout;
+    int valeur_atout;
+    int valeur_non_atout;
+    int valeur_tout_atout;
+    int valeur_sans_atout;
+    int valeur;
+    char affichage[10];
+};
+
+struct Contrat
+{
+    type_contrat type;
+    int atout;
+    int score_mini;
+    char affichage[30];
+};
+
+typedef struct Contrat Contrat;
+
+typedef struct Carte Carte;
+
+struct Joueur
+{
+    char nom[20];
+    Carte carte[8];
+    Contrat contrat;
+};
+
+typedef struct Joueur Joueur;
+
+struct Equipe
+{
+    Joueur joueur[2];
+    int score;
+};
+
+typedef struct Equipe Equipe;
