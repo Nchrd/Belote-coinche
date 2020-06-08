e5094b14bf56dc6d1883cd77eeac5d8af6dac4e7
Noé Echard <65755297+Th3PaSt3qu3@users.noreply.github.com>
2020-06-08 16:57:06 +0200

    Create fonctions.h

---
+++ fonctions.h
@@ -0,0 +1,8 @@
+void initialisation_paquet_total(Carte *paquet_total);
+void initialisation_joueurs(Carte *paquet_total, Joueur *joueur, char *nom_joueur);
+void melange(Carte *);
+void affichage_contrat(Contrat *contrat);
+void compteur_manche(int compteur);
+void affichage_partie(int choix, Joueur *joueur, Contrat *contrat);
+void enchere_surenchere();
+void valeur_cartes();
