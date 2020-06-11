d233f7523a331c014122feb129062e61d81e3eff
Noé Echard <65755297+Th3PaSt3qu3@users.noreply.github.com>
2020-06-11 17:12:38 +0200

    Update liste_fonctions.h

--- liste_fonctions.h
+++ liste_fonctions.h
@@ -1,6 +1,8 @@
 void initialisation_paquet_total(Carte *paquet_total);
 void initialisation_joueurs(Carte *paquet_total, Joueur *joueur, char *nom_joueur);
+void retour_paquet_total(Carte *paquet_total, Joueur *joueur, char *nom_joueur);
 void melange(Carte *);
 void affichage_contrat(Contrat *contrat);
 void compteur_manche(int compteur);
-void affichage_enchere(int choix, Joueur *joueur, Contrat *contrat);
+void affichage_enchere(Joueur *joueur, Contrat *contrat, int compteur_passe);
+void premier_tour(Contrat *contrat, Joueur *joueur, int compteur);
